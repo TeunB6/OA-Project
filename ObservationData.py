779e0977e3f42a891ecd3399f68a9d308ec2823e
T. Boersma <tboersma@virgo11.intra.astro.rug.nl>
2025-05-22 10:58:16 +0200

    Basic DataReduction

--- ObservationData.py
+++ ObservationData.py
@@ -16,6 +16,7 @@ class ObservationData:
     def refresh_dir(self):
         directory = []
         for f in os.listdir(self.directory_path):
+
             with fits.open(os.path.join(self.directory_path, f)) as hdul:
                 header = hdul[0].header
 
@@ -29,10 +30,25 @@ class ObservationData:
                 directory.append(row)
         
         self.directory_frame = pd.DataFrame(directory)
-       
+    
+    def add_file(self, f):
+        with fits.open(os.path.join(self.directory_path, f)) as hdul:
+                header = hdul[0].header
+
+                row = {"FILENAME" : os.path.basename(f),
+                "OBJECT" : header.get("OBJECT", None),
+                "FILTER" : header.get("FILTER", None),
+                "IMAGETYP" : header.get("IMAGETYP", None),
+                "DATE-OBS" : header.get("DATE-OBS", None),
+                "EXP-TIME" : header.get("EXP-TIME", None),
+                "JD" : header.get("JD", None)}
+                
+        self.directory_frame = pd.concat([self.directory_frame, pd.DataFrame([row])], ignore_index=True)
+            
+    
     def load_data(self, expression) -> np.ndarray:
         files = self.filter(expression, replace=False)
-        data = np.array([fits.getdata(os.path.join(data_dir, f)) for f in files["FILENAME"]
+        data = np.array([fits.getdata(os.path.join(self.directory_path, f)) for f in files["FILENAME"]])
         return np.dstack(data)
     
     @staticmethod
@@ -60,3 +76,14 @@ class ObservationData:
     
     def load_csv(self, file_name: str) -> None:
         self.directory_frame = pd.read_csv(file_name)
+        
+
+class Corrector:
+    def __init__(self, master_bias, flat_dict):
+        self.bias = master_bias
+        self.flat = flat_dict
+    
+
+    def correct(self, data: np.ndarray, fil: str):
+        return (data - self.bias) / self.flat[fil]
+        
